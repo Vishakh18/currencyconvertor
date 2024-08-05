ef6e297d5baee3066e9cbd4d56adca3fc690426e
Vishakh18 <vishakhsharma844@gmail.com>
2024-08-05 12:57:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CurrencyConvertor from "./component/CurrencyConverter";
+
+function App() {
+  return (
+    <div className="min-h-screen flex-col flex  items-center justify-center ">
+      <div className="container">
+        <CurrencyConvertor />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/CurrencyConverter.jsx
@@ -0,0 +1,127 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import CurrencyDropdown from "./dropdown/Dropdown";
+import { HiArrowsRightLeft } from "react-icons/hi2";
+
+const CurrencyConverter = () => {
+  const [currencies, setCurrencies] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+  const [converting, setConverting] = useState(false);
+
+  const fetchCurrencies = async () => {
+    try {
+      const res = await fetch("https://api.frankfurter.app/currencies");
+      const data = await res.json();
+
+      setCurrencies(Object.keys(data));
+    } catch (error) {
+      alert("Error Fetching", error);
+    }
+  };
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
+
+  const convertCurrency = async () => {
+    if (!amount) return;
+    setConverting(true);
+    try {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+
+      setConvertedAmount(data.rates[toCurrency] + " " + toCurrency);
+    } catch (error) {
+      alert("Error Fetching", error);
+    } finally {
+      setConverting(false);
+    }
+  };
+
+  const swapCurrencies = () => {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
+
+  return (
+    <div className="max-w-xl mx-auto p-5   rounded-lg shadow-blue-200 shadow-2xl  bg-gradient-to-r from-purple-400 via-blue-500 to-violet-400">
+      <h2 className="mb-5 text-2xl font-semibold text-gray-700">
+        Currency Converter
+      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 items-end">
+        <CurrencyDropdown
+          currencies={currencies}
+          title="From:"
+          currency={fromCurrency}
+          setCurrency={setFromCurrency}
+        />
+
+        <div className="flex justify-center -mb-5 sm:mb-0">
+          <button
+            onClick={swapCurrencies}
+            className="p-2 bg-blue-400 rounded-full cursor-pointer hover:bg-blue-600"
+          >
+            <HiArrowsRightLeft className="text-xl text-white" />
+          </button>
+        </div>
+        <CurrencyDropdown
+          currencies={currencies}
+          currency={toCurrency}
+          setCurrency={setToCurrency}
+          title="To:"
+        />
+      </div>
+
+      <div className="mt-4">
+        <label
+          htmlFor="amount"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Amount:
+        </label>
+        <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          type="number"
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1"
+        />
+      </div>
+      <div className="mt-4">
+        <label
+          htmlFor="amount"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Converted Amount:
+        </label>
+        <input
+          value={convertedAmount}
+          type="text"
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1"
+        />
+      </div>
+
+      <div className="flex justify-center mt-6">
+        <button
+          onClick={convertCurrency}
+          className={`px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
+          ${converting ? "" : ""}`}
+        >
+          {converting ? (
+            <div className=" py-2">
+              <div className="w-8 h-8  border-4 border-blue-200 rounded-full animate-spin"></div>
+            </div>
+          ) : (
+            "Convert"
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyConverter;
